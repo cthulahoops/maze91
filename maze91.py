06f9a5e32470c2989f647d4e14b20402593ee571
Adam Kelly <adam@cthulahoops.org>
2024-10-11 16:56:31 +0100

    Working python port of maze91.bas.

--- maze91.py
+++ maze91.py
@@ -3,23 +3,14 @@ import time
 import sys
 import os
 
-# ANSI escape sequences
-CLEAR_SCREEN = "\033[2J"
-CURSOR_HOME = "\033[H"
-HIDE_CURSOR = "\033[?25l"
-SHOW_CURSOR = "\033[?25h"
+from qb import Screen, WHITE, BLUE, YELLOW, RED, RESET, get_key, SHOW_CURSOR
 
-# Color codes
-WHITE = "\033[97m"
-BLUE = "\033[94m"
-YELLOW = "\033[93m"
-RED = "\033[91m"
-RESET = "\033[0m"
+screen = Screen(80, 25)
 
-# Game characters
-WALL = '#'
+# Game constants
+WALL = '█'
 YOU = '☺'
-GEM = '<'
+GEM = '*'
 ENEMY = '☻'
 DOTS = '·'
 
@@ -29,29 +20,8 @@ score, gems = 0, 0
 mazeshape = []
 skill = 0
 
-def locate(row, col):
-    print(f"\033[{row};{col}H", end='')
-
 def cls():
-    print(CLEAR_SCREEN + CURSOR_HOME, end='')
-
-def rand():
-    global enex, eney, d
-    flag = 0
-    while flag == 0:
-        c = random.randint(1, 4)
-        if c == 1 and get_screen(eney, enex - 1) != WALL:
-            bleft()
-            flag = 1
-        elif c == 2 and get_screen(eney, enex + 1) != WALL:
-            bright()
-            flag = 1
-        elif c == 3 and get_screen(eney - 1, enex) != WALL:
-            bup()
-            flag = 1
-        elif c == 4 and get_screen(eney + 1, enex) != WALL:
-            bdown()
-            flag = 1
+    screen.clear()
 
 def init():
     global wall, you, gem, enemy, dots, mazeshape, score, gems, enex, eney, youx, youy, skill
@@ -61,21 +31,21 @@ def init():
 
     maze1 = [
         "###############################",
-        "#......#<..#.....##############",
+        "#......#<..#.#...##############",
         "######.###.#.#.#............###",
-        "#....#...#.#.#.############.###",
-        "####.###.#.#.#.##........#...##",
-        "#....#...#...#.<.#.####.###.###",
-        "#.####.#######.##.#.#...#<...##",
-        "#.#............##.#.#.####.####",
-        "#.#.###.###.#.#.#<#.#...<#....#",
-        "#.#.#.......#.#.#.###.#######.#",
-        "#.<.###.###.#.#...............#",
+        "#......#...#.#.############.###",
+        "####.###.#.#.#.##.......#.....#",
+        "#........#...#.#<.#.###.###.###",
+        "#.####.#######.##.#.#...#<....#",
+        "#.#.............#.#.#.#####.###",
+        "#.#.###.###.#.#.#<#.#..<#.....#",
+        "#.#.#.....#.#.#.###.#######.###",
+        "#.#.###.###.#.#...............#",
         "#.#.........#.###############.#",
-        "#...#.#.#.#.#.................#",
-        "#####.#.#.#.#.#################",
-        "#.....#.#.#.#.#...............#",
-        "#.#<#.#.#.#.#.#<............#.#",
+        "#...#.#.#.###.#...............#",
+        "#####.#.#.#...#.###############",
+        "#...#.#.#.#.###.#.............#",
+        "#.#<#.#.#.#.#...#<..........#.#",
         "#.###.#.#.#.#.###############.#",
         "#.....#<#<#<#.................#",
         "###############################"
@@ -122,82 +92,100 @@ def init():
 
 def drawn():
     for n, row in enumerate(mazeshape):
-        locate(n + 2, 3)
+        screen.locate(n + 2, 3)
         for char in row:
             if char == '#':
-                print(WHITE + WALL + RESET, end='')
+                screen.color(WHITE)
+                screen.write(WALL)
             elif char == '.':
-                print(BLUE + DOTS + RESET, end='')
+                screen.color(BLUE)
+                screen.write(DOTS)
             elif char == '<':
-                print(YELLOW + GEM + RESET, end='')
+                screen.color(YELLOW)
+                screen.write(GEM)
             elif char == ' ':
-                print(' ', end='')
-    locate(youy, youx)
-    print(YELLOW + YOU + RESET, end='')
-    locate(eney, enex)
-    print(RED + ENEMY + RESET, end='')
-    sys.stdout.flush()
+                screen.write(' ')
+    screen.locate(youy, youx)
+    screen.color(YELLOW)
+    screen.write(YOU)
+    screen.locate(eney, enex)
+    screen.color(RED)
+    screen.write(ENEMY)
     time.sleep(0.05)
-
-def get_screen(y, x):
-    return mazeshape[y-2][x-3]
-
-def set_screen(y, x, char):
-    row = list(mazeshape[y - 2])
-    row[x - 3] = char
-    mazeshape[y - 2] = ''.join(row)
 
 def game():
     global youx, youy, enex, eney, score, gems
     while True:
         scoreboard()
         ex, ey, yx, yy = enex, eney, youx, youy
-        locate(eney, enex)
-        print(RED + ENEMY + RESET, end='')
-        locate(youy, youx)
-        print(YELLOW + YOU + RESET, end='')
-        sys.stdout.flush()
+        screen.locate(youy, youx)
+        screen.color(YELLOW)
+        screen.write(YOU)
+        screen.locate(eney, enex)
+        screen.color(RED)
+        screen.write(ENEMY)
         time.sleep(0.05)
 
-        if sys.stdin.read(1) == '\x1b':
-            if sys.stdin.read(1) == '[':
-                key = sys.stdin.read(1)
-                if key == 'B':
-                    up()
-                elif key == 'A':
-                    down()
-                elif key == 'D':
-                    left()
-                elif key == 'C':
-                    right()
+        key = get_key()
+        if key == 'UP':
+            up()
+        elif key == 'DOWN':
+            down()
+        elif key == 'LEFT':
+            left()
+        elif key == 'RIGHT':
+            right()
+        elif key == 'ESC':
+            break
 
         if random.randint(1, skill) == 1:
             baddymove()
         else:
             rand()
 
-        locate(yy, yx)
-        print(' ', end='')
-        locate(ey, ex)
-        d = get_screen(ey, ex)
-        if d == GEM:
-            print(YELLOW + d + RESET, end='')
-        elif d == DOTS:
-            print(BLUE + d + RESET, end='')
+        screen.locate(yy, yx)
+        screen.write(' ')
+        screen.locate(ey, ex)
+        d = mazeshape[ey - 2][ex - 3]
+        if d == "<":
+            screen.color(YELLOW)
+            screen.write(GEM)
+        elif d == ".":
+            screen.color(BLUE)
+            screen.write(DOTS)
         else:
-            print(d, end='')
-        sys.stdout.flush()
+            screen.write(d)
+
+def rand():
+    global enex, eney, d
+    flag = 0
+    while flag == 0:
+        c = random.randint(1, 4)
+        if c == 1 and screen.get(eney, enex - 1) != WALL:
+            bleft()
+            flag = 1
+        elif c == 2 and screen.get(eney, enex + 1) != WALL:
+            bright()
+            flag = 1
+        elif c == 3 and screen.get(eney - 1, enex) != WALL:
+            bup()
+            flag = 1
+        elif c == 4 and screen.get(eney + 1, enex) != WALL:
+            bdown()
+            flag = 1
+
 
 def scoreboard():
-    locate(1, 1)
-    print(f"Score: {score} ", end='')
-    locate(1, 20)
-    print(YELLOW + GEM * gems + RESET, end='')
-    sys.stdout.flush()
+    screen.locate(1, 1)
+    screen.color(WHITE)
+    screen.write(f"Score: {score} ")
+    screen.locate(1, 20)
+    screen.color(YELLOW)
+    screen.write(GEM * gems)
 
 def up():
     global youy, gems, score
-    p = get_screen(youy + 1, youx)
+    p = screen.get(youy + 1, youx)
     if p != WALL:
         if p == GEM:
             gems += 1
@@ -212,7 +200,7 @@ def up():
 
 def down():
     global youy, gems, score
-    p = get_screen(youy - 1, youx)
+    p = screen.get(youy - 1, youx)
     if p != WALL:
         if p == GEM:
             gems += 1
@@ -227,7 +215,7 @@ def down():
 
 def left():
     global youx, gems, score
-    p = get_screen(youy, youx - 1)
+    p = screen.get(youy, youx - 1)
     if p != WALL:
         if p == GEM:
             gems += 1
@@ -242,7 +230,7 @@ def left():
 
 def right():
     global youx, gems, score
-    p = get_screen(youy, youx + 1)
+    p = screen.get(youy, youx + 1)
     if p != WALL:
         if p == GEM:
             gems += 1
@@ -266,16 +254,16 @@ def baddymove():
 
     for _ in range(10):
         c = random.randint(1, 4)
-        if c == 1 and downgo and get_screen(eney + 1, enex) != WALL:
+        if c == 1 and downgo and screen.get(eney + 1, enex) != WALL:
             bdown()
             return
-        elif c == 2 and upgo and get_screen(eney - 1, enex) != WALL:
+        elif c == 2 and upgo and screen.get(eney - 1, enex) != WALL:
             bup()
             return
-        elif c == 3 and rightgo and get_screen(eney, enex + 1) != WALL:
+        elif c == 3 and rightgo and screen.get(eney, enex + 1) != WALL:
             bright()
             return
-        elif c == 4 and leftgo and get_screen(eney, enex - 1) != WALL:
+        elif c == 4 and leftgo and screen.get(eney, enex - 1) != WALL:
             bleft()
             return
     rand()
@@ -283,81 +271,89 @@ def baddymove():
 def bdown():
     global eney
     eney += 1
-    if get_screen(eney, enex) == ENEMY:
+    if screen.get(eney, enex) == YOU:
         die()
 
 def bup():
     global eney
     eney -= 1
-    if get_screen(eney, enex) == ENEMY:
+    if screen.get(eney, enex) == YOU:
         die()
 
 def bleft():
     global enex
     enex -= 1
-    if get_screen(eney, enex) == ENEMY:
+    if screen.get(eney, enex) == YOU:
         die()
 
 def bright():
     global enex
     enex += 1
-    if get_screen(eney, enex) == ENEMY:
+    if screen.get(eney, enex) == YOU:
         die()
 
 def die():
     time.sleep(0.5)
-    cls()
-    locate(11, 1)
-    print(RED + ENEMY + RESET, end='')
-    locate(11, 2)
-    print(YELLOW + ENEMY + RESET, end='')
-    locate(1, 1)
-    print(WHITE + "He's got you -Shoot him-" + RESET)
+    screen.clear()
+    screen.locate(11, 1)
+    screen.color(RED)
+    screen.write(ENEMY)
+    screen.locate(11, 2)
+    screen.color(YELLOW)
+    screen.write(YOU)
+    screen.locate(1, 1)
+    screen.color(WHITE)
+    screen.write("He's got you -Shoot him-")
     time.sleep(1)
     for _ in range(gems * 10):
         print('\a', end='')
         sys.stdout.flush()
-    print("Oh no You didn't get all the power gems")
-    print("Your out of ammo")
-    print("run")
+    screen.write("\nOh no You didn't get all the power gems")
+    screen.write("\nYour out of ammo")
+    screen.write("\nrun")
     time.sleep(1)
     for n in range(3, 31):
-        locate(11, n - 1)
-        print(RED + '░' + RESET, end='')
-        print(YELLOW + ENEMY + RESET, end='')
-        print('\a', end='')
+        screen.locate(11, n - 1)
+        screen.color(WHITE)
+        screen.write(' ')
+        screen.color(YELLOW)
+        screen.write(YOU)
         sys.stdout.flush()
         time.sleep(0.05)
-    locate(11, 30)
-    print(RED + '░' + RESET, end='')
+    screen.locate(11, 30)
+    screen.color(RED)
+    screen.write('░')
     time.sleep(1)
-    cls()
-    print(WHITE + f"He got you\nScore: {score}\nGems: {gems}" + RESET)
+    screen.clear()
+    screen.color(WHITE)
+    screen.write(f"He got you\nScore: {score}\nGems: {gems}")
     sys.exit()
 
 def win():
     cls()
     time.sleep(0.5)
-    print(WHITE + "You have all the power gems, after him" + RESET)
+    screen.color(WHITE)
+    screen.write("You got all the power gems, after him")
     time.sleep(1)
     for n in range(1, 38):
-        locate(11, n)
-        print(YELLOW + ' ' + ENEMY + RESET, end='')
-        print(RED + '░' + ENEMY + RESET, end='')
-        print('\a', end='')
+        screen.locate(11, n)
+        screen.write(" ")
+        screen.color(YELLOW)
+        screen.write(YOU)
+        screen.color(RED)
+        screen.write(ENEMY)
         sys.stdout.flush()
         time.sleep(0.05)
-    locate(11, 40)
-    print('░', end='')
+    screen.locate(11, 40)
+    screen.write('░')
     cls()
-    print(WHITE + "The world is safe, you are a hero")
+    print("The world is safe, you are a hero")
     print("and I'm Lawrence of Arabia!")
     print("The world is never safe!")
-    print("and my granny was more of a hero than you!" + RESET)
+    print("and my granny was more of a hero than you!")
     sys.exit()
 
 if __name__ == "__main__":
-    print(HIDE_CURSOR, end='')
     import termios
     import tty
     old_settings = termios.tcgetattr(sys.stdin)
